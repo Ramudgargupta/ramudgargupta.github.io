402d3e1a5cedf84a80e507ba867b9ece06420254
Ramudgargupta <ramudgargupta802@gmail.com>
2026-07-07 16:22:54 +0530

    Update rating.js

--- rating.js
+++ rating.js
@@ -1,7 +1,6 @@
 // ===============================
 // SUPABASE CONNECT
 // ===============================
-alert("rating.js version 2");
 const supabaseUrl = "https://puazbdmudwwvxeloirvu.supabase.co";
 
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB1YXpiZG11ZHd3dnhlbG9pcnZ1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzODQxODQsImV4cCI6MjA5ODk2MDE4NH0.67uiJ3OKpSU3Xy1d0ckDtw_c9EpitjAGzeXEVTrB1cg";
